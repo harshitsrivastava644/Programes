b2e9ab25cc62fefa041daf448bf492a0a829f6e9
harshitsrivastava644 <121300311+harshitsrivastava644@users.noreply.github.com>
2023-02-16 01:20:11 +0530

    Add files via upload

---
+++ To swap the first n character of two strings.py
@@ -0,0 +1,15 @@
+#program to swap the first n character of two string
+l = True
+while l == True:
+    str1 = input("Please Enter First String : ")
+    str2 = input("Please Enter Second String : ")
+    n = int(input("Enter no. of chars to swap : "))
+    x=str1[0:n]
+
+    str1=str1.replace(str1[0:n],str2[0:n])
+
+    str2=str2.replace(str2[0:n],x)
+
+    print("Your first string has become :- ",str1)
+    print("Your second string has become :- ",str2)
+    break
